2bed387e64e90e0775c3da4cb20e11d00028dd9c
doppl3r <deben3@gmail.com>
2015-12-16 12:43:30 -0700

    finished initial layout

--- php/mail.php
+++ php/mail.php
@@ -14,17 +14,15 @@ $mail = new PHPMailer();
 //Set who the message is to be sent from
 $mail->setFrom($_REQUEST['email'], $_REQUEST['name']);
 //Set who the message is to be sent to
-$mail->addAddress('info@varitecsolutions.com');
 $mail->addAddress('deben3@gmail.com');
 //Set the subject line
 $mail->Subject = 'New Contact form filled out!';
 //Read an HTML message body from an external file, convert referenced images to embedded,
 //convert HTML into a basic plain-text alternative body
-$message =  '<div style="padding: 64px; background-color: #9ebc42; border-bottom: 4px solid #83a90d; max-width: 480px; margin: 0 auto; text-align: center;">'.
+$message =  '<div style="padding: 64px; background-color: #000; max-width: 480px; margin: 0 auto; text-align: center;">'.
             '<p style="color: #fff; font-size: 24px;"><strong>Somebody wants to contact you!</strong></p>'.
-            '<p style="color: #fff; font-size: 24px;"><strong>Name:</strong> '.$_REQUEST['name'].'</p>'.
+            '<p style="color: #fff; font-size: 24px;"><strong>Name:</strong> '.$_REQUEST['fname'].' '.$_REQUEST['lname'].'</p>'.
             '<p style="color: #fff; font-size: 24px;"><strong>Email Address:</strong> '.$_REQUEST['email'].'</p>'.
-            '<p style="color: #fff; font-size: 24px;"><strong>Phone:</strong> '.$_REQUEST['usrtel'].'</p>'.
             '<p style="color: #fff; font-size: 24px;"><strong>Comment:</strong> '.$_REQUEST['comment'].'</p>'.
             '</div>';
 $mail->msgHTML($message);
