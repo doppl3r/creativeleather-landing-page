4651144ed9e3bb9cd20f1b7e8a84748e0ff8673c
Jacob DeBenedetto <deben3@gmail.com>
2016-05-03 11:19:55 -0700

    simplify tracker code

--- js/custom-GA.js
+++ js/custom-GA.js
@@ -4,12 +4,5 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-71719750-2', 'auto');
-ga('send', 'pageview');
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-5273646-1', 'auto');
+ga('create', 'UA-5273646-1', 'auto', 'clientTracker');
 ga('send', 'pageview');
